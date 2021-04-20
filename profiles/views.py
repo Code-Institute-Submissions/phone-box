ec7942dd98f589d391932d8ba0185197b9857eea
Phillip A Pearson <phillip.pearson@live.co.uk>
2021-04-20 15:46:19 +0100

    started checkout app, started a user dashboard in profiles app within the profile.html, create a usermailinglist class model.. im going to fail

--- profiles/views.py
+++ profiles/views.py
@@ -5,25 +5,6 @@ from .models import UserProfile
 from .forms import UserProfileForm
 
 
-@login_required
 def profile(request):
-    """ Display the user's profile. """
-    profile = get_object_or_404(UserProfile, user=request.user)
-
-    if request.method == 'POST':
-        form = UserProfileForm(request.POST, instance=profile)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Profile updated successfully')
-        else:
-            messages.error(request, 'Update failed. Please ensure the form is valid.')
-    else:
-        form = UserProfileForm(instance=profile)
-
-    template = 'profiles/profile.html'
-    context = {
-        'form': form,
-        'on_profile_page': True
-    }
-
-    return render(request, template, context)
+    """ a view to return the user dashboard """
+    return render(request, 'profiles/profile.html')
