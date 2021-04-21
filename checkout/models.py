00a781792a8c423a3389b8abdb9d4ad058578f5a
Phillip A Pearson <phillip.pearson@live.co.uk>
2021-04-21 19:04:46 +0100

    wrote models for product and order

--- checkout/models.py
+++ checkout/models.py
@@ -1,12 +1,44 @@
+import uuid
+
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django.db.models import Sum
+from django.conf import settings
+from profiles.models import UserProfile
 
 # Create your models here.
-"""
-class Order():
 
-class Product():
-    
-"""
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    street_address1 = models.CharField(max_length=80, null=False, blank=False)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
+    def _generate_order_number(self):
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    donation_name = models.CharField(max_length=200, null=True, blank=True)
+    donation_price = models.IntegerField(max_length=3, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
