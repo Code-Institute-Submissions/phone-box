77681295434ee187bad1740a25aba53a726248e7
Phillip A Pearson <phillip.pearson@live.co.uk>
2021-04-24 21:23:15 +0100

    ran, python manage.py collectstatic'

--- charitybox/settings.py
+++ charitybox/settings.py
@@ -20,9 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '9riyarthbsh6qwwq5b5uio)&x5p9onb5-w+w(p!s+1u*z!(jwx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+##DEBUG = True
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['*']
@@ -168,7 +169,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
