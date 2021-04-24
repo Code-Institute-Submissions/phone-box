84744fd8f9abc29867be132cde2ec1315c3485d2
Phillip A Pearson <phillip.pearson@live.co.uk>
2021-04-24 19:30:09 +0100

    made secret key secret, and set debug to true if 'DEVELOPMENT' is in environment.

--- charitybox/settings.py
+++ charitybox/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9riyarthbsh6qwwq5b5uio)&x5p9onb5-w+w(p!s+1u*z!(jwx'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = []
 
