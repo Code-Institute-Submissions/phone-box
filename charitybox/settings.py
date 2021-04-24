3c89152f6f1cabe6342c3a236d4b29d10126447b
Phillip A Pearson <phillip.pearson@live.co.uk>
2021-04-24 20:53:28 +0100

    tried something different for collecstatic to work

--- charitybox/settings.py
+++ charitybox/settings.py
@@ -168,7 +168,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
