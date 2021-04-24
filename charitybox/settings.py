6491f2a52a284e65c39d52a2baabc12aedfec1f1
Phillip A Pearson <phillip.pearson@live.co.uk>
2021-04-24 20:06:27 +0100

    tried configuring heroku settings

--- charitybox/settings.py
+++ charitybox/settings.py
@@ -165,3 +165,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+## Configure Django app for Heroku
+import django_heroku
+django_heroku.settings(locals())
