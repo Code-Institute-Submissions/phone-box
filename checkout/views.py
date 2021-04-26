79ad1ecd181dd8e03841db4c192344851b9a99af
Phillip A Pearson <phillip.pearson@live.co.uk>
2021-04-26 22:07:43 +0100

    tried creating a session where item data is sent to the checkout.html template

--- checkout/views.py
+++ checkout/views.py
@@ -11,6 +11,25 @@ from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
 
 
-def checkout(request):
+def checkout(request, item_id):
 
-    return render(request, 'checkout/checkout.html')
+    donation = request.session.get('donation', {})
+
+    request.session['donation'] = donation[item_id]
+    print(request.session['donation'])
+    return render(request, 'checkout/checkout.html', donation)
+
+def pay_now(request):
+
+    donation = request.session.get('donation', {})
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IcwMYIXOx9uZSSgsHfRJknAxd9ghJVcy7xI83IdyiUfj7EZ7cjsdBwJBUvMNGKRZSb1bwN288Z7p83aLTb0Opoo00hIm3whET',
+        'client_secret': 'test client secret',
+    }
+
+    return render(request, template, context)
