212ba4c6a7e2bfa37ec14b170445c955fc168ac2
Phillip A Pearson <phillip.pearson@live.co.uk>
2021-05-12 20:39:47 +0100

    altered some html and css elements in donation history, checkout success and profile dashboard, and created a SubscriptionMalingList class in models.py

--- base/views.py
+++ base/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.db.models import Sum
+from profiles.models import donationHistory
 
 
 def index(request):
-    """ A view to return the index page"""
+    """ A view to return the index page, and render out the sum total of all
+    donations made"""
+
     return render(request, 'home/index.html')
--- checkout/models.py
+++ checkout/models.py
@@ -3,7 +3,16 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
-from profiles.models import UserProfile
+from profiles.models import UserProfile, User
 
 # Create your models here.
 
+class SubscriptionMailingList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    email = models.CharField(max_length=255, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
